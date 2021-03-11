303b55b4a32fe5fbb598aa75d2faaec1d56d5934
Kim Yuen <kim_man@live.com>
2021-03-12 06:15:41 +1100

    prepare for deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const connectDB = require("./config/db");
 
 const app = express();
@@ -14,5 +15,15 @@ app.get("/", (req, res) =>
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
+
+//serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set a static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
